3242551e3ec4a74b315f20362785411b64160166
Code Sandbox <hello@codesandbox.io>
2021-06-01 01:49:41 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.b71c19560b996d63a503f873d64bc19f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22c48c21ce3af669646aba4796c2181e",
+    "url": "https://zpsceo.github.io/csb-w3wcc/index.html"
+  },
+  {
+    "revision": "07f711d15b76a8ef0cd4",
+    "url": "https://zpsceo.github.io/csb-w3wcc/static/css/2.1b3d2ff7.chunk.css"
+  },
+  {
+    "revision": "6c89f0d1bd8a770e6df3",
+    "url": "https://zpsceo.github.io/csb-w3wcc/static/css/main.cdd15a01.chunk.css"
+  },
+  {
+    "revision": "07f711d15b76a8ef0cd4",
+    "url": "https://zpsceo.github.io/csb-w3wcc/static/js/2.82e20065.chunk.js"
+  },
+  {
+    "revision": "464fef3691779b3926634327df24dae8",
+    "url": "https://zpsceo.github.io/csb-w3wcc/static/js/2.82e20065.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c89f0d1bd8a770e6df3",
+    "url": "https://zpsceo.github.io/csb-w3wcc/static/js/main.14a09480.chunk.js"
+  },
+  {
+    "revision": "0a26c9770c2bd23a941f",
+    "url": "https://zpsceo.github.io/csb-w3wcc/static/js/runtime-main.cacd32ff.js"
+  }
+]);
